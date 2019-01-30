2331d52c45e4a11fb33686ae7072aa9c25577fed
Joanna Hoang <hoangj21@up.edu>
2019-01-30 04:40:56 -0800

    Travis build fail
    Changed functions.test.js to call functions on (2,2) instead of (1,2) to fail on purpose

--- js/functions.test.js
+++ js/functions.test.js
@@ -1,7 +1,7 @@
 const functions = require('./functions');
 test('adds 1 + 2 to equal 3', () => {
-  expect(functions(1, 2)).toBe(3);
+  expect(functions(2, 2)).toBe(3);
 });
 test('object assignment', () => {
-    expect(functions(1,2)).toEqual(3);
+    expect(functions21,2)).toEqual(3);
   });
